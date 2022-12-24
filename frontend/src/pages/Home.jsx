8746675b2d63a6a39c2d042b5b6bdf6478b3cc9b
pablodiaz <derivadas@protonmail.com>
2022-12-24 11:34:38 +0100

    styling home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import ListaPostales from "../components/ListaPostales";
-import "./HomeStyles.css";
+import "./../styles/Home.css";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function Home() {
-  return (
+return (
     <React.Fragment>
-      <div className="d-flex flex-row p-4 bg-primary align-items-center justify-content-between border-bottom border-dark">
+      <div className="d-flex flex-row p-4 align-items-center justify-content-between border-bottom border-dark navbar-bg">
         <div className="d-flex flex-row justify-content-between w-100 align-items-center">
-          <h1 className="text-white">Postales Cadarso 2022</h1>
-            <a href="https://github.com/" target="_blank">
-              <img height="50" src="github-mark-white.png"></img>
+          <div className="title">
+            <h1 className="text-white title-text">🎄Postales Cadarso 2022🎄</h1>
+          </div>
+          <a href="https://github.com/" target="_blank" rel="noreferrer">
+            <img height="50" src="github-mark-white.png" alt="logo github"></img>
           </a>
         </div>
-      </div> 
-      <ListaPostales/>
+      </div>
+      <div className="mainContent">
+        <div className="p-3" id="subtitle">
+          <p>Hola! Bienvenido a esta página para pasaros mis postales de Navidad de este año. Como estoy en Noruega de Erasmus no pude haceros
+            a todos una postal escrita, así que decidí crear esta web para ello (y para aprender a hacer una de paso). Espero que os gusten mucho! </p>
+          <p className="author">Pablo</p>
+        </div>
+        <ListaPostales/>
+      </div>
+      <Carousel fade interval={10000} keyboard wrap className="backgroundContent">
+          <Carousel.Item>
+            <img className="d-block w-100" src="cadarso1.jpg" alt="cadars dia"/>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src="cadarso2.jpg" alt="cadarso noche"/>
+            </Carousel.Item>
+      </Carousel>
     </React.Fragment>
+    
   );
 }
